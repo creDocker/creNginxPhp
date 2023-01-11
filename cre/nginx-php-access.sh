4e05d7de1abd1b4203feaef649ae8b9f83f276aa
KMicha <michael.kahle@yahoo.de>
2023-01-11 12:19:48 +0100

    add weekly access report generation

---
+++ cre/nginx-php-access.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+
+#https://geekflare.com/monitor-analyze-access-logs-goaccess/
+
+goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
+cp -f /cre/$CRE_PHP_ROOT/access.html /cre/$CRE_PHP_ROOT/access_w$(date +"%V").html 
+
+#https://www.learn2torials.com/a/how-to-rotate-nginx-logs
+# /etc/logrotate.conf  'su root syslog' -> 'su root adm'   
+
