0131358f9185e31413c866de6fe6e12be5bdfd38
KMicha <michael.kahle@yahoo.de>
2023-01-13 10:38:04 +0100

    move weekly reports to subdir

--- cre/nginx-php-access.sh
+++ cre/nginx-php-access.sh
@@ -3,7 +3,8 @@
 #https://geekflare.com/monitor-analyze-access-logs-goaccess/
 
 goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
-cp -f /cre/$CRE_PHP_ROOT/access.html /cre/$CRE_PHP_ROOT/access_w$(date +"%V").html 
+mkdir -p /cre/$CRE_PHP_ROOT/access
+cp -f /cre/$CRE_PHP_ROOT/access.html /cre/$CRE_PHP_ROOT/access/access_w$(date +"%V").html 
 
 #https://www.learn2torials.com/a/how-to-rotate-nginx-logs
 # /etc/logrotate.conf  'su root syslog' -> 'su root adm'   
