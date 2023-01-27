d45ed3dd19dd057d48065ea7951f0d6556ea462a
KMicha <michael.kahle@yahoo.de>
2023-01-27 14:02:34 +0100

    try version check this way

--- cre/nginx-php-access.sh
+++ cre/nginx-php-access.sh
@@ -4,7 +4,6 @@
 
 ##if [ "$CRE_VERSION" --eq "2016.0" ]
 if [ $(echo " $CRE_VERSION < 2018.0" | bc) -eq 1 ] ; then
-then
   goaccess -f /var/log/nginx/php.access.log   --log-format="%h %^[%d:%^] \"%r\" %s %b \"%R\" \"%u\"" --date-format=%d/%b/%Y --time-format=%T --log-format=COMBINED > /cre/$CRE_PHP_ROOT/access.html
 else
   goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
