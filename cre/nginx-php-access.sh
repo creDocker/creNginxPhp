0412d2a3c783673d5e5bebefa63b51825679df79
KMicha <michael.kahle@yahoo.de>
2023-01-27 13:45:01 +0100

    handle old verson of goaccess

--- cre/nginx-php-access.sh
+++ cre/nginx-php-access.sh
@@ -2,7 +2,13 @@
 
 #https://geekflare.com/monitor-analyze-access-logs-goaccess/
 
-goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
+if [ "$CRE_VERSION" == "2016.0" ]
+then
+  goaccess -f /var/log/nginx/php.access.log   --log-format="%h %^[%d:%^] \"%r\" %s %b \"%R\" \"%u\"" --date-format=%d/%b/%Y --time-format=%T --log-format=COMBINED > /cre/$CRE_PHP_ROOT/access.html
+else
+  goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
+fi
+
 mkdir -p /cre/$CRE_PHP_ROOT/access
 cp -f /cre/$CRE_PHP_ROOT/access.html /cre/$CRE_PHP_ROOT/access/access_w$(date +"%V").html 
 
