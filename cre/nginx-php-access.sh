2529b629593f95fa9ad7251bfe5fa275eedde849
KMicha <michael.kahle@yahoo.de>
2023-01-27 15:35:37 +0100

    adapt version for goaccess

--- cre/nginx-php-access.sh
+++ cre/nginx-php-access.sh
@@ -4,7 +4,7 @@
 
 ## if [ /var/log/nginx/php.access.log -nt /cre/$CRE_PHP_ROOT/access.html ] ; then
 if [ -f /var/log/nginx/php.access.log ]; then
-  if [ $(echo " $CRE_VERSION < 2018.0" | bc) -eq 1 ] ; then
+  if [ $(echo " $CRE_VERSION < 2020.0" | bc) -eq 1 ] ; then
     goaccess -f /var/log/nginx/php.access.log   --log-format="%h %^[%d:%^] \"%r\" %s %b \"%R\" \"%u\"" --date-format=%d/%b/%Y --time-format=%T --log-format=COMBINED > /cre/$CRE_PHP_ROOT/access.html
   else
     goaccess /var/log/nginx/php.access.log -o /cre/$CRE_PHP_ROOT/access.html --anonymize-ip --log-format=COMBINED
