28b459d46905d0ecb513627fd9b7e60838074c80
KMicha <michael.kahle@yahoo.de>
2023-03-21 08:42:46 +0100

    potential use of geo-ip

--- cre/nginx-php-access.sh
+++ cre/nginx-php-access.sh
@@ -2,6 +2,14 @@
 
 #https://geekflare.com/monitor-analyze-access-logs-goaccess/
 
+
+#Country: 3MB
+## https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=YOUR_LICENSE_KEY&suffix=tar.gz
+# City: 30MB
+## https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=YOUR_LICENSE_KEY&suffix=tar.gz
+
+## https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=YOUR_LICENSE_KEY&suffix=tar.gz
+
 ## if [ /var/log/nginx/php.access.log -nt /cre/$CRE_PHP_ROOT/access.html ] ; then
 if [ -f /var/log/nginx/php.access.log ]; then
   if [ $(echo " $CRE_VERSION < 2020.0" | bc) -eq 1 ] ; then
@@ -18,3 +26,10 @@ fi
 #https://www.learn2torials.com/a/how-to-rotate-nginx-logs
 # /etc/logrotate.conf  'su root syslog' -> 'su root adm'   
 
+
+
+
+
+#goaccess php2.access.log -o access.html --anonymize-ip --log-format=COMBINED --enable-geoip=mmdb --geoip-database /home/kmicha/Downloads/GeoLite2-Country_20230131/GeoLite2-Country.mmdb
+
+#goaccess php2.access.log -o access.html --anonymize-ip --log-format=COMBINED --enable-geoip=mmdb --geoip-database /home/kmicha/Downloads/GeoLite2-ASN_20230131/GeoLite2-ASN.mmdb
